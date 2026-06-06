ce637c799df5e1ac99b90e77c1baf6a0339cfebc
Manling <dsv0320@autuni.ac.nz>
2026-06-07 00:11:13 +1200

    Added new questions, added embedded database (derby)
    Also created some empty classes to be filled in later.

--- src/main/java/Question/QuestionPool.java
+++ src/main/java/Question/QuestionPool.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 public class QuestionPool {
     //hashMap to keep track of question index and question object
-    private Map<Integer, Question> questionPool = new HashMap<>();
+    private Map<Integer, Question> questionPool;
 //    private Random random = new Random();
 
     public QuestionPool() {
@@ -40,6 +40,8 @@ public class QuestionPool {
     
     //method to insert questions into questionPool map
     //note: question attribute format (questionText, explanation, answer, id)
+    
+    //Week 1: Java Recap
     private void loadQuestions() {
         questionPool.put(1, new MultiChoiceQuestion(
             """
@@ -89,6 +91,7 @@ public class QuestionPool {
             """,
             3, 3));
 
+        //Week 2: Java collections, File IO
         questionPool.put(4, new MultiChoiceQuestion(
             """
             Which collection interface has no order and no duplicate elements?
@@ -202,6 +205,7 @@ public class QuestionPool {
             """,
             3, 10));
                 
+        //Week 3: modern programming languages (AI prompting), Apache projects
         questionPool.put(11, new MultiChoiceQuestion(
             """
             Autonomous AI that sets its own goals and decisions with little to 
@@ -242,7 +246,7 @@ public class QuestionPool {
         questionPool.put(13, new MultiChoiceQuestion(
             """
             What type of prompting involves providing at least one example to 
-            guide the AI what the output should look like?
+            guide the AI on what the output should look like?
             1) Few-shot prompting
             2) Problem decomposition
             3) Zero-shot prompting
@@ -282,6 +286,7 @@ public class QuestionPool {
             """,
             3, 15));
 
+        //Week 4: OOAD, SOLID design
         questionPool.put(16, new MultiChoiceQuestion(
             """
             Which of these diagrams is a structure diagram?
@@ -374,17 +379,20 @@ public class QuestionPool {
             """,
             2, 21));
        
+        //Week 5: multi-threading
         questionPool.put(22, new MultiChoiceQuestion(
             """
-            What is the library required to use threads?
+            What is one of the libraries used for multi-threading?
             1) java.util.Thread
             2) java.io.Thread
             3) java.lang.Thread
             """,
             """
-            Threads are the smallest unit of execution in a program. Processes
-            (actively running program) run at least one thread. Use 
-            java.lang.Thread to use the thread library.
+            Threads are the smallest unit of execution in a process (actively
+            running program). Multi-threading is when a process runs multiple
+            threads simultaneously. java.lang.Thread and java.lang.Runnable do
+            not have to be manually imported because java.lang library is 
+            automatically included in every Java file.
             """,
             3, 22));
         
@@ -423,6 +431,7 @@ public class QuestionPool {
             """,
             2, 24));
         
+        //Week 6: git
         questionPool.put(25, new MultiChoiceQuestion(
             """
             What is a 'branch' in Git?
@@ -466,7 +475,344 @@ public class QuestionPool {
             Git CMD (Git Command Prompt)
             """,
             3, 27));
+    
+        //Week 7: GUI
+        questionPool.put(28, new MultiChoiceQuestion(
+            """
+            What is an event object in a GUI?
+            1) An object that represents a user action
+            2) An object that listens for an event triggered by the user
+            3) An object that starts an event for the user to interact with 
+            """,
+            """
+            When a user interacts with the GUI, such as clicking a button, Java 
+            creates an event object, which stores data about the action, such as
+            the action source and time that the action occurred. A listener 
+            object is what listens for and responds to events. It is inactive 
+            until an event object gets sent to it.
+            """,
+            1, 28));
         
+        questionPool.put(29, new MultiChoiceQuestion(
+            """
+            What method of adding ActionListener is this?
+            
+            myButton.addActionListener( new ActionListener() { 
+              public void actionPerformed (ActionEvent e) {
+                …
+               }
+            }
+            1) Inner class listener
+            2) Anonymous inner listener
+            3) ActionLister implementing JFrame
+            """,
+            """
+            In a Graphical User Interface (GUI), an anonymous inner listener 
+            directly uses the actionPerformed method when adding the 
+            ActionListener to a component. There is no need to make a separate 
+            class and refer to a class name.
+            """,
+            2, 29));
+        
+        questionPool.put(30, new MultiChoiceQuestion(
+            """
+            Which kind of GUI layout involves arranging components in 
+            fixed-length rows and columns?
+            1) GridBagLayout
+            2) CardLayout
+            3) GridLayout
+            """,
+            """
+            GridLayout allows components to be arranged in fixed-length rows 
+            and columns. GridBagLayout is similar to this, except that it allows
+            components to be different lengths.
+            """,
+            3, 30));
+                
+        questionPool.put(31, new MultiChoiceQuestion(
+            """
+            Which GUI component is used to let users select multiple options?
+            1) Combo boxes
+            2) Radio Buttons
+            3) Checkboxes
+            """,
+            """
+            Checkboxes allow multiple selections while Combo boxes and Radio 
+            buttons allow one. Make a Checkbox using this command:
+            JCheckBox checkbox = new JCheckBox('Option text');
+            """,
+            3, 31));
+                 
+        //No Week 8 materials
+        
+        //Week 9: JDBC
+        questionPool.put(32, new MultiChoiceQuestion(
+            """
+            What does SQL stand for?
+            1) Standard Query Language
+            2) Structured Query Language
+            3) Statement Query Language
+            """,
+            """
+            In databases, Structured Query Language (SQL) is used to create 
+            table structures, insert, modify, and delete data from tables, and 
+            retrieve data through queries.
+            """,
+            2, 32));
+                                
+        questionPool.put(33, new MultiChoiceQuestion(
+            """
+            What is one thing that JDBC does not standardise?
+            1) SQL syntax
+            2) Data structure of query results
+            3) Ways to connect to a database
+            """,
+            """
+            Java Database Connectivity (JDBC) is an Application Programming 
+            Interface (API) that connects programmers to databases. It does not 
+            standardise SQL syntax because each Database Management System 
+            (DBMS) has their own syntax, like different dialects. Examples of 
+            DBMS are MySQL, Apache Derby, Oracle.
+            """,
+            1, 33));
+                                        
+        questionPool.put(34, new MultiChoiceQuestion(
+            """
+            What parameter is omitted when connecting to an embedded database?
+            1) Database network address
+            2) Database name
+            3) Driver type
+            """,
+            """
+            Since the database is embedded, it is directly integrated into the 
+            Java project folder. There is no need to provide the network 
+            address, so the url for connection will look like this:
+            String url = 'jdbc:derby:DBname; create=true';
+            """,
+            1, 34));
+                                                
+        questionPool.put(35, new MultiChoiceQuestion(
+            """
+            In databases, when calling next() on a ResultSet object for the 
+            first time, where is the cursor placed?
+            1) Second row in table
+            2) First row in table
+            3) Second to last row in table
+            """,
+            """
+            ResultSet acts as an iterator (cursor) that goes through a table 
+            row-by-row. It goes forward by default (top to bottom). Initially, 
+            the cursor is placed before the first row. Call next() before 
+            trying to access the first row's data.
+            """,
+            2, 35));
+                              
+        //Week 10: Design patterns
+        questionPool.put(36, new MultiChoiceQuestion(
+            """
+            At what level of development do idiom patterns occur?
+            1) System Level
+            2) Subsystem Level
+            3) Code level
+            """,
+            """
+            Idioms in programming occur at code level. Idioms are a common or 
+            popular way to express a task. Example: using an iterator instead 
+            of a for loop to go through elements in a collection.
+            """,
+            3, 36));
+        
+        questionPool.put(37, new MultiChoiceQuestion(
+            """
+            What type of design pattern is the Factory pattern?
+            1) Structural
+            2) Behavioural
+            3) Creational
+            """,
+            """
+            The Factory pattern defines creating an interface with concrete 
+            subclasses, which allows the right subclass object to be returned 
+            based on a user's input. Example: returning bananas out of many 
+            fruit objects. This comes under the Creational design pattern, 
+            which involves common ways to create objects and instantiate 
+            classes.
+            """,
+            3, 37));
+        
+        questionPool.put(38, new MultiChoiceQuestion(
+            """
+            Which pattern has one class that handles interactions between other 
+            classes?
+            1) Singleton
+            2) Mediator
+            3) Adapter
+            """,
+            """
+            The Mediator pattern is used to reduce coupling (over-reliance) of 
+            classes. Instead of many classes interacting with each other (which 
+            gets hard to manage), they communicate through the mediator. The 
+            classes can focus on their responsibilities without worrying about 
+            the responsibilities of others (separation of concerns).
+            """,
+            2, 38));
+        
+        questionPool.put(39, new MultiChoiceQuestion(
+            """
+            In the Model View Controller (MVC) pattern, which component 
+            manipulates or updates the Model?
+            1) Controller
+            2) View
+            3) Model
+            """,
+            """
+            View displays visual elements to the user in a GUI. When the View 
+            receives a user action, such as clicking a button, it generates an 
+            event that triggers the corresponding method in the Controller. The 
+            Controller updates the Model, which holds the business logic. 
+            Business logic is rules that control how data is accessed and 
+            updated.
+            """,
+            1, 39));
+        
+        //Week 11: validation and verification, JUnit framework
+        questionPool.put(40, new MultiChoiceQuestion(
+            """
+            A human action that causes an incorrect result is known as...
+            1) Mistake
+            2) Fault
+            3) Failure
+            """,
+            """
+            Faults are an incorrect step in a computer program. Failures are 
+            incorrect results or outputs. Since computers do not make mistakes, 
+            human error leads to faults then failures.
+            """,
+            1, 40));
+        
+        questionPool.put(41, new MultiChoiceQuestion(
+            """
+            In High Level Testing, which kind of testing checks the stability 
+            of a program and how well it handles unexpected values?
+            1) Usability testing
+            2) Stress testing
+            3) System Function testing
+            """,
+            """
+            Stress testing gives the program extreme values, often to breaking 
+            points to study the outcome. Usability testing involves getting 
+            users to try the program to observe ease of use. System Function 
+            testing checks the overall program and sees if the program matches 
+            its functional specifications.
+            """,
+            2, 41));
+        
+        questionPool.put(42, new MultiChoiceQuestion(
+            """
+            Regarding the Unit Testing Framework, which is false about a unit?
+            1) A unit is the smallest testable component in a program
+            2) A unit is usually a method
+            3) A unit depends on other components
+            """,
+            """
+            Units should not depend on other components (other classes, 
+            methods, database, etc.) for the sake of testing the unit in 
+            isolation. The point of unit testing is to ensure that a particular 
+            unit component is passing test cases and operating as expected.
+            """,
+            3, 42));
+        
+        questionPool.put(43, new MultiChoiceQuestion(
+            """
+            What is the verdict of this JUnit test case? The value calculated 
+            by Multiply class is 15.0
+            
+            @Test
+            public void test() {
+              Multiply m = new Multiply();
+              m.calculate(2.3, 6.54);
+              double actual = m.getResult();
+              double expected = 15.042;
+              Assert.assertEquals(actual, expected, 0.01);
+            }
+            1) Pass
+            2) Error
+            3) Fail
+            """,
+            """
+            The delta value (0.01) is the acceptable range for rounding in a 
+            float or double. The test case results in a fail because the 
+            difference between the expected and actual value is outside delta's 
+            range (15.032 to 15.052). Check if the actual value falls within 
+            delta's range using Math.abs(expected - actual) <= delta
+            = |15.042 - 15.0| = 0.042 
+            0.042 > 0.01 = Fail
+            """,
+            3, 43));
+        
+        //Week 12: Code smells
+        questionPool.put(44, new MultiChoiceQuestion(
+            """
+            What is Bad Code Smells?
+            1) Code that contains bugs or errors which is considered smelly
+            2) Signs of bad design or implementation choices
+            3) Bad code that is hard to read
+            """,
+            """
+            Smelly code does not cause the program to crash, but it can reduce 
+            readability and maintainability of the code. It is called 'Bad Code 
+            Smells' because a surface stench signals a deeper problem, like a 
+            stinky house from leaking pipes. Smelly code hints that there may 
+            be design choices that leads to technical debt (making the cheapest 
+            change in the present that leads to higher cost in the future).
+            """,
+            2, 44));
+        
+        questionPool.put(45, new MultiChoiceQuestion(
+            """
+            What is the correct definition for Bloaters in Bad Code Smells?
+            1) Classes and methods that have grown too large 
+            2) Classes and methods with high coupling
+            3) Classes and methods that use Object-Oriented principles 
+            incorrectly
+            """,
+            """
+            Bloaters are large classes and methods. They are harder to 
+            understand and maintain due to the long length, and they typically 
+            accumulate overtime when no effort is made to refactor 
+            (shorten/split) them. Methods longer than 10 lines of code might be 
+            at risk of becoming a bloater.
+            """,
+            1, 45));
+        
+        questionPool.put(46, new MultiChoiceQuestion(
+            """
+            Which kind of code smell includes Shotgun Surgery?
+            1) Change Preventers
+            2) Bloaters
+            3) Object-Orientation Abusers
+            """,
+            """
+            Change Preventers occurs when classes and methods have high 
+            coupling (high dependency) with each other. Changing one part of 
+            the code can lead to changing many other parts. Shotgun Surgery is 
+            specifically when a modification requires changing multiple classes.
+            """,
+            1, 46));
+        
+        questionPool.put(47, new MultiChoiceQuestion(
+            """
+            What does it mean when a class has low cohesion?
+            1) The class depends less on other classes or methods
+            2) The class juggles too many different tasks 
+            3) The class is focused on its task
+            """,
+            """
+            Low cohesion is when a class is unfocused on its actual job due to 
+            handling a variety of tasks. The aim for a program is to have high 
+            cohesion, where the class is focused on its job. High cohesion 
+            follows Single Responsibility Principle (SRP).
+            """,
+            2, 47));
 
     }
 }
