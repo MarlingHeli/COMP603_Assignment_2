05851b72b56252d257fb72db77b56c0fcd7d867d
Manling <dsv0320@autuni.ac.nz>
2026-06-13 10:56:57 +1200

    Added table for questions, split up large DB class into multiple classes

---
+++ src/main/java/Persistence/DatabaseDAO.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistence;
+
+
+/**
+ *
+ * @author hmarl
+ */
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Base DAO class.
+ * Provides common database functionality.
+ */
+public abstract class DatabaseDAO {
+
+    protected Connection connection;
+
+    public DatabaseDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * Each DAO creates its own table.
+     */
+    public abstract void createTable();
+    
+//    public abstract boolean usernameExists(String username);
+
+    /**
+     * Check whether a table already exists.
+     *
+     * @param table Table name
+     * @return true if table exists
+     */
+    public boolean checkTableExists(String table) {
+
+        try {
+            DatabaseMetaData dbmd = connection.getMetaData();
+            //get table type only
+            String[] types = {"TABLE"};
+
+            try (
+                ResultSet resultSet = dbmd.getTables(null, null, null, types)) {
+
+                while (resultSet.next()) {
+
+                    String tableName = resultSet.getString("TABLE_NAME");
+
+                    //check if names match
+                    if (table.equalsIgnoreCase(tableName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        catch (SQLException ex) {
+            System.out.println("Failed to read database metadata");
+        }
+
+        return false;
+    }
+}
--- src/main/java/Persistence/QuizRecord.java
+++ src/main/java/Persistence/QuizRecord.java
@@ -2,16 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package Persistence;
 
 /**
  *
  * @author hmarl
  */
-public interface ModelListener {
-    public void onExistingUser();
-    public void onNewUser();
-    public void onEnterAnswer();
-    public void onScoreUpdated();
-    
+
+import Model.User;
+import Model.QuizSession;
+
+public interface QuizRecord {
+    void saveGame(QuizSession session);
+    QuizSession loadGame(User user);
 }
